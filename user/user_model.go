835859f6b1ef67d21d43a492c3346146caff087a
momin-mostafa <mominmostafa.g@gmail.com>
2025-11-28 21:44:31 +0600

    refactor: update user model fields and validation in request handler

--- user/user_model.go
+++ user/user_model.go
@@ -6,25 +6,31 @@ import (
 
 type User struct {
 	gorm.Model
-	Name  string
-	Phone string
-	FB_ID string
-	Email string
+	FullName       string
+	Phone          string
+	StudentID      string
+	PersonalEmail  string
+	HashedPassword string
+	UnivesityEmail string
 }
 
 type CreateUserRequest struct {
-	Name  string `json:"name"`
-	Phone string `json:"phone"`
-	FB_ID string `json:"fb_id"`
-	Email string `json:"email"`
+	FullName       string `json:"full_name"`
+	Phone          string `json:"phone"`
+	StudentID      string `json:"student_id"`
+	PersonalEmail  string `json:"personal_email"`
+	HashedPassword string `json:"password"`
+	UnivesityEmail string `json:"university_email"`
 }
 
 func (req *CreateUserRequest) createUser() User {
 	user := User{
-		Name:  req.Name,
-		Phone: req.Phone,
-		FB_ID: req.FB_ID,
-		Email: req.Email,
+		FullName:       req.FullName,
+		Phone:          req.Phone,
+		StudentID:      req.StudentID,
+		PersonalEmail:  req.PersonalEmail,
+		HashedPassword: req.HashedPassword,
+		UnivesityEmail: req.UnivesityEmail,
 	}
 
 	return user
